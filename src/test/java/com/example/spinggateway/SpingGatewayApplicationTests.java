9c14598e2afccfdbfc789c1d15dbf73b2bcfd1d1
Nikita Veko <n.veko@glovisrus.com>
2022-10-07 15:35:50 +0300

    init

---
+++ src/test/java/com/example/spinggateway/SpingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spinggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
